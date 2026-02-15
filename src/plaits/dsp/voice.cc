e65017d4ce5e826fda8f10451b07af11f6255b83
Mark Washeim <blueprint@poetaster.de>
2026-02-15 15:32:38 +0100

    [Fix] formatting

--- src/plaits/dsp/voice.cc
+++ src/plaits/dsp/voice.cc
@@ -73,11 +73,13 @@ void Voice::Init(BufferAllocator* allocator) {
 
   out_post_processor_.Init();
   aux_post_processor_.Init();
+
   decay_envelope_.Init();
   lpg_envelope_.Init();
 
   trigger_state_ = false;
   previous_note_ = 0.0f;
+
   trigger_delay_.Init(trigger_delay_line_);
 }
 
@@ -108,6 +110,7 @@ void Voice::Render(
   const int six_op_2_index = 18;
 
   int engine_index = engine_quantizer_.Process(patch.engine, engine_cv_);
+
   Engine* e = engines_.get(engine_index);
 
   if (engine_index != previous_engine_index_ || reload_user_data_) {
@@ -134,20 +137,28 @@ void Voice::Render(
   }
 
   EngineParameters p;
+  
   bool rising_edge = trigger_state_ && !previous_trigger_state;
   float note = (modulations.note + previous_note_) * 0.5f;
   previous_note_ = modulations.note;
   const PostProcessingSettings& pp_s = e->post_processing_settings;
 
-  p.trigger = modulations.trigger_patched
-                ? (rising_edge ? TRIGGER_RISING_EDGE : TRIGGER_LOW) | (trigger_state_ ? TRIGGER_HIGH : TRIGGER_LOW)
-                : TRIGGER_UNPATCHED;
+  if (modulations.trigger_patched) {
+    p.trigger = rising_edge ? TRIGGER_RISING_EDGE : TRIGGER_LOW;
+  } else {
+    p.trigger = TRIGGER_UNPATCHED;
+  }
 
-  const float short_decay = (200.0f * kBlockSize) / kSampleRate * SemitonesToRatio(-96.0f * patch.decay);
+  const float short_decay = (200.0f * kBlockSize) / kSampleRate * 
+      SemitonesToRatio(-96.0f * patch.decay);
+
   decay_envelope_.Process(short_decay * 2.0f);
 
-  float compressed_level = 1.3f * modulations.level / (0.3f + fabsf(modulations.level));
-  CONSTRAIN(compressed_level, 0.0f, 1.0f);
+  //float compressed_level = 1.3f * modulations.level / (0.3f + fabsf(modulations.level));
+  //CONSTRAIN(compressed_level, 0.0f, 1.0f);
+   const float compressed_level = max(
+       1.3f * modulations.level / (0.3f + fabsf(modulations.level)),
+       0.0f);
   p.accent = modulations.level_patched ? compressed_level : 0.8f;
 
   bool use_internal_envelope = modulations.trigger_patched;
@@ -160,30 +171,50 @@ void Voice::Render(
   if (engine_index == 7) {
     internal_env_amp = 2.0f - p.harmonics * 6.0f;
     CONSTRAIN(internal_env_amp, 0.0f, 1.0f);
-    speech_engine_.set_prosody_amount(!modulations.trigger_patched || modulations.frequency_patched ? 0.0f : patch.frequency_modulation_amount);
-    speech_engine_.set_speed(!modulations.trigger_patched || modulations.morph_patched ? 0.0f : patch.morph_modulation_amount);
-  }  //else if (engine_index == 7) {
-     // if (modulations.trigger_patched && !modulations.timbre_patched) {
-  //  internal_env_amp_timbre = 0.0f;
-  //  chiptune_engine_.set_envelope_shape(patch.timbre_modulation_amount);
-  // } else {
-  //   chiptune_engine_.set_envelope_shape(ChiptuneEngine::NO_ENVELOPE);
-  // }
-  //}
+    speech_engine_.set_prosody_amount(
+            !modulations.trigger_patched || modulations.frequency_patched ? 
+            0.0f : patch.frequency_modulation_amount);
+    speech_engine_.set_speed(
+            !modulations.trigger_patched || modulations.morph_patched ? 
+            0.0f : patch.morph_modulation_amount);
+  }
 
-  // Apply modulations
-  p.note = ApplyModulations(patch.note + note, patch.frequency_modulation_amount, modulations.frequency_patched,
-                            modulations.frequency, use_internal_envelope,
-                            internal_env_amp * decay_envelope_.value() * decay_envelope_.value() * 48.0f,
-                            1.0f, -119.0f, 120.0f);
+  p.note = ApplyModulations(
+          patch.note + note, 
+          patch.frequency_modulation_amount, 
+          modulations.frequency_patched,
+          modulations.frequency, 
+          use_internal_envelope, 
+          internal_env_amp * \
+            decay_envelope_.value() * decay_envelope_.value() * 48.0f, 
+            1.0f, 
+            -119.0f, 
+            120.0f);
 
-  p.timbre = ApplyModulations(patch.timbre, patch.timbre_modulation_amount, modulations.timbre_patched,
-                              modulations.timbre, use_internal_envelope,
-                              internal_env_amp_timbre * decay_envelope_.value(), 0.0f, 0.0f, 1.0f);
+  p.timbre = ApplyModulations(
+          patch.timbre, 
+          patch.timbre_modulation_amount, 
+          modulations.timbre_patched, 
+          modulations.timbre, 
+          use_internal_envelope, 
+          internal_env_amp * \
+            decay_envelope_.value() * decay_envelope_.value() * 48.0f, 
+          //internal_env_amp_timbre * decay_envelope_.value(), 
+          //decay_envelope_.value(),
+          0.0f, 
+          0.0f, 
+          1.0f);
 
-  p.morph = ApplyModulations(patch.morph, patch.morph_modulation_amount, modulations.morph_patched,
-                             modulations.morph, use_internal_envelope,
-                             internal_env_amp * decay_envelope_.value(), 0.0f, 0.0f, 1.0f);
+  p.morph = ApplyModulations(
+          patch.morph, 
+          patch.morph_modulation_amount, 
+          modulations.morph_patched, 
+          modulations.morph, 
+          use_internal_envelope, 
+          internal_env_amp * decay_envelope_.value(), 
+          0.0f, 
+          0.0f, 
+          1.0f);
 
   bool already_enveloped = pp_s.already_enveloped;
   e->Render(p, out_buffer_, aux_buffer_, size, &already_enveloped);
@@ -192,7 +223,8 @@ void Voice::Render(
 
   if (!lpg_bypass) {
     const float hf = patch.lpg_colour;
-    const float decay_tail = (20.0f * kBlockSize) / kSampleRate * SemitonesToRatio(-72.0f * patch.decay + 12.0f * hf) - short_decay;
+    const float decay_tail = (20.0f * kBlockSize) / kSampleRate * 
+        SemitonesToRatio(-72.0f * patch.decay + 12.0f * hf) - short_decay;
 
     if (modulations.level_patched) {
       lpg_envelope_.ProcessLP(compressed_level, short_decay, decay_tail, hf);
@@ -205,11 +237,27 @@ void Voice::Render(
   }
 
   // Post-processing
-  out_post_processor_.Process(pp_s.out_gain, lpg_bypass, lpg_envelope_.gain(), lpg_envelope_.frequency(),
-                              lpg_envelope_.hf_bleed(), out_buffer_, &frames->out, size, 2);
+  out_post_processor_.Process(
+          pp_s.out_gain, 
+          lpg_bypass, 
+          lpg_envelope_.gain(), 
+          lpg_envelope_.frequency(), 
+          lpg_envelope_.hf_bleed(), 
+          out_buffer_, 
+          &frames->out, 
+          size, 
+          2);
 
-  aux_post_processor_.Process(pp_s.aux_gain, lpg_bypass, lpg_envelope_.gain(), lpg_envelope_.frequency(),
-                              lpg_envelope_.hf_bleed(), aux_buffer_, &frames->aux, size, 2);
+  aux_post_processor_.Process(
+          pp_s.aux_gain, 
+          lpg_bypass, 
+          lpg_envelope_.gain(), 
+          lpg_envelope_.frequency(), 
+          lpg_envelope_.hf_bleed(), 
+          aux_buffer_, 
+          &frames->aux, 
+          size, 
+          2);
 }
 
 }  // namespace plaits
